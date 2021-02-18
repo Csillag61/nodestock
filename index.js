f386dfbc8dcf28fcc5dac945476901b494d58994
Csillag61 <csillagok@gmail.com>
2021-02-17 20:06:27 -0500

    startin up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const exphbs= require('express-handlebars');
+const path= require('path');
+
+const PORT =  process.env.PORT || 5000;
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//set handlebar routs
+app.get('/', function (req, res) {
+    res.render('home');
+});
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.listen(PORT, ()=> console.log(`Server Listening on port ${PORT}`))
